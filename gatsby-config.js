c7ec44aa483e89b0b38c9333c3d6668ef6843ae6
Pedro Piñera <pedro@ppinera.es>
2020-01-20 18:46:23 +0100

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,18 +11,17 @@ module.exports = {
     `gatsby-plugin-theme-ui`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `María José Salmerón`,
+        short_name: `mjsesalm`,
+        start_url: `/`,
+        theme_color: `hsl(10, 80%, 50%)`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
