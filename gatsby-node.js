d1de8d918726213d5ab15000ff8b32e8c5f254fe
Pedro Piñera <pedro@ppinera.es>
2020-01-20 19:14:45 +0100

    Add list of blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,30 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const slugify = require("slug")
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `Mdx`) {
+    const fileNode = getNode(node.parent)
+
+    if (fileNode.dir.includes("content/posts/")) {
+      const filename = createFilePath({ node, getNode, basePath: `posts` })
+
+      const postName = filename
+      const [, date, title] = postName
+        .split("/")[1]
+        .match(/^([\d]{4}-[\d]{2}-[\d]{2})-{1}(.+)$/)
+
+      const slug = `/blog/${slugify([date].join("-"), "/")}/${title}/`
+
+      createNodeField({ node, name: `type`, value: "blog-post" })
+      createNodeField({ node, name: `slug`, value: slug })
+      createNodeField({ node, name: `date`, value: date })
+      createNodeField({ node, name: `path`, value: fileNode.relativePath })
+    } else {
+      const filename = createFilePath({ node, getNode })
+      createNodeField({ node, name: `slug`, value: filename })
+    }
+  }
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,12 +7,77 @@ import { graphql, useStaticQuery } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Styled } from "theme-ui"
 import Meta from "../components/meta"
+import moment from "moment"
 
-const IndexPage = () => (
-  <Layout>
-    <Meta title="Blog" />
-    <Styled.h1>Blog</Styled.h1>
-  </Layout>
-)
+function usePosts() {
+  const {
+    allFile: { nodes: posts },
+  } = useStaticQuery(graphql`
+    {
+      allFile(
+        filter: { base: { eq: "post.mdx" } }
+        sort: { fields: childMdx___fields___date, order: DESC }
+      ) {
+        nodes {
+          childMdx {
+            frontmatter {
+              title
+              tags
+            }
+            fields {
+              date
+              slug
+            }
+            excerpt(pruneLength: 300)
+            body
+          }
+        }
+      }
+    }
+  `)
+  return posts.map(post => post.childMdx)
+}
+
+const Post = ({ post }) => {
+  const publishedDateString = moment(post.fields.date).format("MMMM Do YYYY")
+  return (
+    <article sx={{ mb: 3 }}>
+      <header>
+        <Styled.h2 sx={{ mb: 2 }}>{post.frontmatter.title}</Styled.h2>
+        <Styled.div sx={{ fontSize: 2, color: "gray" }}>
+          Published on {publishedDateString} {` · `}
+          {post.frontmatter.tags.map(tag => (
+            <span
+              sx={{
+                mr: 2,
+              }}
+            >
+              {`#${tag}`}
+            </span>
+          ))}
+        </Styled.div>
+      </header>
+      <main>
+        <Styled.p>{post.excerpt}</Styled.p>
+      </main>
+      <footer>
+        <Link to={post.fields.slug}>Read on...</Link>
+      </footer>
+    </article>
+  )
+}
+
+const IndexPage = () => {
+  const posts = usePosts()
+  return (
+    <Layout>
+      <Meta title="Blog" />
+      <Styled.h1>Blog</Styled.h1>
+      {posts.map((post, index) => {
+        return <Post post={post} key={index} />
+      })}
+    </Layout>
+  )
+}
 
 export default IndexPage
